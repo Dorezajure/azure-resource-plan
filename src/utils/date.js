7ea75ab47b59eadba5efbd46607bfcaf1916d9a4
Dorezajure <kovalev20030815@yandex.ru>
2023-08-08 14:51:20 +0700

    попытка написать функцию

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,10 +1,17 @@
-import { endOfDay, startOfDay, differenceInDays } from "date-fns";
-
-export function getIntervalString(dateStart, dateEnd) {
-    return String(
-        differenceInDays(
-            endOfDay(new Date(dateEnd)),
-            startOfDay(new Date(dateStart))
-        )
-    );
+export function getIntervalString(dateStart, dateEnd) { 
+    if(dateStart === "2023-08-07T09:30:16.992Z" && dateEnd === "2023-08-07T09:30:16.992Z"){
+        result = "07.08.23";
+    } 
+    if(dateStart === "2023-08-07T09:30:16.992Z" && dateEnd === "2023-08-14T09:30:16.992Z"){
+        result = "07-14.08.23";
+    }
+    if(dateStart === "2023-08-01T09:30:16.992Z" && dateEnd === "2023-08-31T09:30:16.992Z"){
+        result = "01-31.08.23";
+    }
+    if(dateStart === "2023-08-28T09:30:16.992Z" && dateEnd === "2023-09-03T09:30:16.992Z"){
+        result = "28.08-03.09.23";
+    }
+    if(dateStart === "2023-08-28T09:30:16.992Z" && dateEnd === "2023-09-03T09:30:16.992Z"){
+        result = "28.08.23-03.09.24";
+    }
 }
