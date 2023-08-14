186ad8538c70e50299e040d46e72fa5eb38ef8e3
Dorezajure <kovalev20030815@yandex.ru>
2023-08-14 16:39:17 +0700

    Создание функции по подсчету интервалов дат

--- src/Header/Select.js
+++ src/Header/Select.js
@@ -1,21 +1,39 @@
 import React from "react";
 import "../sass/body.scss";
 import { useRef } from "react";
+import { eachDayOfInterval, eachWeekOfInterval, eachMonthOfInterval } from 'date-fns';
 
 const Select = () => {
     // useRef
     // Для каждого элемента свой реф 
     const nameRef = useRef(null);
     const projectRef = useRef(null);
-    const dateRef = useRef(null);
+    const dateStartRef = useRef(null);
+    const dateEndRef = useRef(null);
     const dayRef = useRef(null);
 
     const handleClick = () => {
-        // Выполнить часть после вопросительного знака только в том случае если не undefined
-        console(nameRef.current?.value());
-        console(projectRef.current?.value());
-        console(dateRef.current?.value());
-        console(dayRef.current?.value());
+        if(dayRef.current?.value === "День"){
+            // В рассмотрении по дням 
+            return console.log(eachDayOfInterval({
+                start: new Date(dateStartRef.current?.value),
+                end: new Date(dateEndRef.current?.value)
+            }));
+        }
+        if(dayRef.current?.value === "Неделя"){
+            // В рассмотрении по неделям 
+            return console.log(eachWeekOfInterval({
+                start: new Date(dateStartRef.current?.value),
+                end: new Date(dateEndRef.current?.value)
+            }));
+        }
+        if(dayRef.current?.value === "Месяц"){
+            // В рассмотрении по месяцам  
+            return console.log(eachMonthOfInterval({
+                start: new Date(dateStartRef.current?.value),
+                end: new Date(dateEndRef.current?.value)
+            }));
+        }
     }
 
     return (
@@ -35,12 +53,12 @@ const Select = () => {
                     <option value="Проект 3">Проект 3</option>
                     <option value="Проект 4">Проект 4</option>
                 </select>
-                <form className="select" ref={dateRef}>
+                <div className="select">
                         <label htmlFor="fromDate">От: </label>
-                        <input type="date" className="fromDate" required="" />
+                        <input type="date" className="fromDate" required=""  ref={dateStartRef}/>
                         <label htmlFor="toDate">До: </label>
-                        <input type="date" className="toDate" required="" />
-                </form>
+                        <input type="date" className="toDate" required="" ref={dateEndRef}/>
+                </div>
                 <select className="select" ref={dayRef}>
                     <option value="День">День</option>
                     <option value="Неделя">Неделя</option>
