dd7803e68cd5daee288618463f7f76f4d11d1681
Dorezajure <kovalev20030815@yandex.ru>
2023-08-16 16:48:48 +0700

    реализация алгоритма дат

--- src/Header/Select.js
+++ src/Header/Select.js
@@ -1,71 +1,98 @@
 import React from "react";
 import "../sass/body.scss";
 import { useRef } from "react";
-import { eachDayOfInterval, eachWeekOfInterval, eachMonthOfInterval } from 'date-fns';
+import {
+    eachDayOfInterval,
+    eachWeekOfInterval,
+    eachMonthOfInterval,
+} from "date-fns";
+import { getIntervalDate } from "../utils/date";
 
 const Select = () => {
     // useRef
-    // Для каждого элемента свой реф 
+    // Для каждого элемента свой реф
     const nameRef = useRef(null);
     const projectRef = useRef(null);
     const dateStartRef = useRef(null);
     const dateEndRef = useRef(null);
     const dayRef = useRef(null);
 
+    // Функция для определения временных промежутков
     const handleClick = () => {
-        if(dayRef.current?.value === "День"){
-            // В рассмотрении по дням 
-            return console.log(eachDayOfInterval({
-                start: new Date(dateStartRef.current?.value),
-                end: new Date(dateEndRef.current?.value)
-            }));
+        if (dayRef.current?.value === "День") {
+            // В рассмотрении по дням
+            return getIntervalDate(
+                eachDayOfInterval({
+                    start: new Date(dateStartRef.current?.value),
+                    end: new Date(dateEndRef.current?.value),
+                })
+            );
         }
-        if(dayRef.current?.value === "Неделя"){
-            // В рассмотрении по неделям 
-            return console.log(eachWeekOfInterval({
-                start: new Date(dateStartRef.current?.value),
-                end: new Date(dateEndRef.current?.value)
-            }));
+        if (dayRef.current?.value === "Неделя") {
+            // В рассмотрении по неделям
+            return getIntervalDate(
+                eachWeekOfInterval({
+                    start: new Date(dateStartRef.current?.value),
+                    end: new Date(dateEndRef.current?.value),
+                })
+            );
         }
-        if(dayRef.current?.value === "Месяц"){
-            // В рассмотрении по месяцам  
-            return console.log(eachMonthOfInterval({
-                start: new Date(dateStartRef.current?.value),
-                end: new Date(dateEndRef.current?.value)
-            }));
+        if (dayRef.current?.value === "Месяц") {
+            // В рассмотрении по месяцам
+            return getIntervalDate(
+                eachMonthOfInterval({
+                    start: new Date(dateStartRef.current?.value),
+                    end: new Date(dateEndRef.current?.value),
+                })
+            );
         }
-    }
+    };
 
     return (
-            <div className="row">
-                <select className="select" ref={nameRef}>
-                    <option value="All">Все</option>
-                    <option value="Александр Морозов">Александр Морозов</option>
-                    <option value="Игорь Глухих">Игорь Глухих</option>
-                    <option value="Дмитрий Овчинников">
-                        Дмитрий Овчинников
-                    </option>
-                    <option value="Михаил Прологов">Михаил Прологов</option>
-                </select>
-                <select className="select" ref={projectRef}>
-                    <option value="Проект 1">Проект 1</option>
-                    <option value="Проект 2">Проект 2</option>
-                    <option value="Проект 3">Проект 3</option>
-                    <option value="Проект 4">Проект 4</option>
-                </select>
-                <div className="select">
-                        <label htmlFor="fromDate">От: </label>
-                        <input type="date" className="fromDate" required=""  ref={dateStartRef}/>
-                        <label htmlFor="toDate">До: </label>
-                        <input type="date" className="toDate" required="" ref={dateEndRef}/>
-                </div>
-                <select className="select" ref={dayRef}>
-                    <option value="День">День</option>
-                    <option value="Неделя">Неделя</option>
-                    <option value="Месяц">Месяц</option>
-                </select>
-                <button className="button" onClick={() => {handleClick()}}>Получить данные</button>
+        <div className="row">
+            <select className="select" ref={nameRef}>
+                <option value="All">Все</option>
+                <option value="Александр Морозов">Александр Морозов</option>
+                <option value="Игорь Глухих">Игорь Глухих</option>
+                <option value="Дмитрий Овчинников">Дмитрий Овчинников</option>
+                <option value="Михаил Прологов">Михаил Прологов</option>
+            </select>
+            <select className="select" ref={projectRef}>
+                <option value="Проект 1">Проект 1</option>
+                <option value="Проект 2">Проект 2</option>
+                <option value="Проект 3">Проект 3</option>
+                <option value="Проект 4">Проект 4</option>
+            </select>
+            <div className="select">
+                <label htmlFor="fromDate">От: </label>
+                <input
+                    type="date"
+                    className="fromDate"
+                    required=""
+                    ref={dateStartRef}
+                />
+                <label htmlFor="toDate">До: </label>
+                <input
+                    type="date"
+                    className="toDate"
+                    required=""
+                    ref={dateEndRef}
+                />
             </div>
+            <select className="select" ref={dayRef}>
+                <option value="День">День</option>
+                <option value="Неделя">Неделя</option>
+                <option value="Месяц">Месяц</option>
+            </select>
+            <button
+                className="button"
+                onClick={() => {
+                    handleClick();
+                }}
+            >
+                Получить данные
+            </button>
+        </div>
     );
 };
 
--- src/utils/date.js
+++ src/utils/date.js
@@ -1,7 +1,6 @@
 import { getYear, getMonth, getDate, format } from "date-fns";
 
-export function getIntervalString(dateStart, dateEnd) { 
-
+export function getIntervalString(dateStart, dateEnd) {
     const datesStart = new Date(dateStart);
     // 3 переменные для dateStart
     const dayStart = getDate(datesStart);
@@ -15,30 +14,73 @@ export function getIntervalString(dateStart, dateEnd) {
     const yearEnd = getYear(datesEnd);
 
     // Все данные равны
-    if(dayStart === dayEnd && monthStart === monthEnd && yearStart === yearEnd){
+    if (
+        dayStart === dayEnd &&
+        monthStart === monthEnd &&
+        yearStart === yearEnd
+    ) {
         return format(datesStart, "dd.MM.yy");
     }
 
-    // Разные дни 
-    if(dayStart !== dayEnd && monthStart === monthEnd && yearStart === yearEnd){
+    // Разные дни
+    if (
+        dayStart !== dayEnd &&
+        monthStart === monthEnd &&
+        yearStart === yearEnd
+    ) {
         return format(datesStart, "dd") + "-" + format(datesEnd, "dd.MM.yy");
     }
 
-    // Разные дни и месяцы 
-    if(dayStart !== dayEnd && monthStart !== monthEnd && yearStart === yearEnd){
+    // Разные дни и месяцы
+    if (
+        dayStart !== dayEnd &&
+        monthStart !== monthEnd &&
+        yearStart === yearEnd
+    ) {
         return format(datesStart, "dd.MM") + "-" + format(datesEnd, "dd.MM.yy");
     }
 
-    // Разные года дни и месяцы 
-    if(dayStart !== dayEnd && monthStart !== monthEnd && yearStart !== yearEnd){
-        return format(datesStart, "dd.MM.yy") + "-" + format(datesEnd, "dd.MM.yy");
+    // Разные года дни и месяцы
+    if (
+        dayStart !== dayEnd &&
+        monthStart !== monthEnd &&
+        yearStart !== yearEnd
+    ) {
+        return (
+            format(datesStart, "dd.MM.yy") + "-" + format(datesEnd, "dd.MM.yy")
+        );
     }
 }
 
-// dd.MM.yy 
-// format(new Date(dateStart), "dd.MM.yy") 
+export function getIntervalDate(strings) {
+    // В результате выполнения кода в dates будет содержаться новый массив объектов, где каждый объект будет содержать дату в формате Date и исходную строку из массива strings.
 
-// Диапазон
-// format(new Date(dateStart), "dd") + "-" + format(new Date(dateEnd), "dd.MM.yy") 
-// format(new Date(dateStart), "dd.MM") + "-" + format(new Date(dateEnd), "dd.MM.yy") 
-// format(new Date(dateStart), "dd.MM.yy") + "-" + format(new Date(dateEnd), "dd.MM.yy") 
+    // ЧТО ПЕРЕДАТЬ В MAP?
+    const dates = strings.map((str) => ({}));
+
+    // Содаем изменяемую переменную
+    let current = { datesStart: dates[0].string };
+
+    // Переменная с итоговым интервалом дат
+    const intervals = [current];
+
+    // Данный код анализирует массив dates и на основе разницы между датами создает объекты интервалов, где каждый интервал представлен объектом с начальной и конечной датами. Все эти объекты интервалов добавляются в массив intervals.
+
+    // ПЕРЕПИСАТЬ УСЛОВИЕ
+    for (let i = 1; i < dates.length; i += 1) {
+        if (diff === 1) {
+            // Если разница равна 1 дню, то берем на вывод "dateEnd" в input
+            current.dateEnd = dates[i].string;
+        } else {
+            // Иначе в интервал мы записываем datesStart - равный 1 элементу и dateEnd - равный
+            current = {
+                datesStart: dates[0].string,
+                dateEnd: dates.length - 1,
+            };
+            intervals.push(current);
+        }
+    }
+
+    // Вывод полученного интервала
+    console.log(intervals);
+}
